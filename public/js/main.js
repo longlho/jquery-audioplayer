0180428e572e92966d8d75b692c5581f8b09685b
Long Ho <holevietlong@gmail.com>
2013-01-18 12:05:48 -0500

    update libraries and small tweaks
    use small song files

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+/*jslint laxcomma:true*/
 // jQuery plugins
 $.extend({
   getUrlVars : function(string) {
@@ -25,7 +26,7 @@ $.extend({
 
 // Rudimentary wrapper that acts like a model, but I don't wanna bring Backbone
 // in since this POC is simple.
-var Audio = function ($audio) {
+var JAudio = function ($audio) {
   var _this = this;
   this.$audio = $audio;
 
@@ -45,10 +46,10 @@ var Audio = function ($audio) {
   this.setState = function (state) {
     switch (state) {
       case 'play':
-        this.getDOMAudio().play();
+        this.getDOMJAudio().play();
         break;
       case 'pause':
-        this.getDOMAudio().pause();
+        this.getDOMJAudio().pause();
         break;
     }
     this.$audio.data('state', state);
@@ -64,14 +65,15 @@ var Audio = function ($audio) {
     if (index < 0) index = 0;
     $.getJSON('playlist?song=' + index, null, function (data) {
       _reset.call(_this, index, data.result);
-      if (!options) return;
-      $.isFunction(options.success) && options.success();
+      if (options && $.isFunction(options.success)) {
+        options.success();
+      }
     });
   };
-  this.getDOMAudio = function () {
+  this.getDOMJAudio = function () {
     return $audio.get(0);
   };
-}
+};
 
 var Player = function (div, song) {
 
@@ -89,57 +91,60 @@ var Player = function (div, song) {
       }
     });
     options.next.click(function () {
-      _audio.fetch(_audio.getCurrent() + 1);
+      _audioModel.fetch(_audioModel.getCurrent() + 1);
     });
     options.prev.click(function () {
-      _audio.fetch(_audio.getCurrent() - 1);
+      _audioModel.fetch(_audioModel.getCurrent() - 1);
     });
-  }
-  ,   _setupProgress = function (options) {
+  };
+  
+  var _setupProgress = function (options) {
     var model = options.model
-      , audio = model.getDOMAudio();
-    model.$audio.on('progress', function () {
-      console.log(this.buffered.end(0));
-      console.log(this.duration);
-      var loaded = parseInt(((this.buffered.end(0) / this.duration) * 100) + 3, 10);
-      options.loading.css('width', loaded + '%');
-    });
-    var manualSeek = false;
-    var loaded = false;
-    options.handle.css('top', '-50%');
-    model.$audio.on('timeupdate', function () {
-      var rem = parseInt(audio.duration - audio.currentTime, 10),
-      pos = Math.floor((audio.currentTime / audio.duration) * 100),
-      mins = Math.floor(rem/60, 10),
-      secs = rem - mins * 60;
+      , manualSeek = false
+      , loaded = false
+      , audio = model.getDOMJAudio();
 
-      options.timeLeft.text('-' + mins + ':' + (secs > 9 ? secs : '0' + secs));
-      if (!manualSeek) { options.handle.css({left: pos + '%'}); }
-      if (!loaded) {
-        loaded = true;
-      }
-    });
+    options.handle.css('top', '50%');
+    model.$audio
+      .on('progress', function () {
+        var buffered = this.buffered.end(0);
+        if (!buffered) return;
+        var loaded = parseInt(((buffered / this.duration) * 100) + 3, 10);
+        options.loading.css('width', loaded + '%');
+      })
+      .on('timeupdate', function () {
+        var rem = parseInt(audio.duration - audio.currentTime, 10),
+        pos = Math.floor((audio.currentTime / audio.duration) * 100),
+        mins = Math.floor(rem/60, 10),
+        secs = rem - mins * 60;
+
+        options.timeLeft.text('-' + mins + ':' + (secs > 9 ? secs : '0' + secs));
+        if (!manualSeek) { options.handle.css({left: pos + '%'}); }
+        if (!loaded) {
+          loaded = true;
+        }
+      });
   };
 
 
 
   var $next = $(div + ' button.playback-next')
-  ,   $prev = $(div + ' button.playback-prev')
-  ,   $play = $(div + ' button.playback-play')
-  ,   $trackInfo = $(div + ' p.track-info')
-  ,   $songProgress = $trackInfo.find('.song-progress')
-  ,   $loading = $songProgress.find('.loading')
-  ,   $timeLeft = $trackInfo.find('.timeleft')
-  ,   $slider = $songProgress.find('.ui-slider')
-  ,   $handle = $slider.find('.ui-slider-handle')
-  ,   $title = $(div + ' h1.ui-title')
-  ,   $buttonText = $play.parent().find('.ui-btn-text')
-  ,   $audio = $(div + ' audio');
+    , $prev = $(div + ' button.playback-prev')
+    , $play = $(div + ' button.playback-play')
+    , $trackInfo = $(div + ' p.track-info')
+    , $songProgress = $trackInfo.find('.song-progress')
+    , $loading = $songProgress.find('.loading')
+    , $timeLeft = $trackInfo.find('.timeleft')
+    , $slider = $songProgress.find('.ui-slider')
+    , $handle = $slider.find('.ui-slider-handle')
+    , $title = $(div + ' h1.ui-title')
+    , $buttonText = $play.parent().find('.ui-btn-text')
+    , $audio = $(div + ' audio');
 
-  var _audioModel = new Audio($audio);
+  var _audioModel = new JAudio($audio);
   $audio.on('change:src', function (ev, filename) {
     var filenameArr = filename.split('/')
-    ,   songName = filenameArr[filenameArr.length - 1];
+      , songName = filenameArr[filenameArr.length - 1];
     $title.text(songName);
   });
   $audio.on('change:state', function (ev, state) {
--- server.js
+++ server.js
@@ -1,39 +1,35 @@
+/*jslint laxcomma:true*/
 var express = require('express')
-,   app = express.createServer()
-,   fs = require('fs')
-,   PORT = 8081
-,   MUSIC_PATH = 'music/'
-,   songs;
-
-
-app.use(express.static(__dirname + '/public', { maxAge: 86400000 }));
-app.all('/playlist', function(req, res) {
-    console.log("Request from:" + req.connection.remoteAddress);
-    Controller.handlePlaylist(req, res);
-});
-
+, app = express()
+, fs = require('fs')
+, PORT = 8081
+, MUSIC_PATH = 'music/'
+, songs;
 
 var Playlist = (function () {
-    return {
-        getSong: function(name) {
-            return fs.readdirSync(__dirname + '/public/' + MUSIC_PATH + name);
-        }
+  return {
+    getSong: function(name) {
+      return fs.readdirSync(__dirname + '/public/' + MUSIC_PATH + name);
     }
+  };
 })();
 
-var Controller = (function () {
-    return {
-        handlePlaylist: function(req, res) {
-            if (!songs) {
-                songs = Playlist.getSong(req.param('playlist'));
-            }
-            var index = req.param('song');
-            index && index >= 0 && index < songs.length
-            ? res.send({ 'result': MUSIC_PATH + 'kpop/' + songs[index] })
-            : res.send({ 'filenames': songs });
-        }
+var Controller = {
+  handlePlaylist: function(req, res) {
+    if (!songs) {
+      songs = Playlist.getSong(req.param('playlist'));
     }
-})();
+    var index = req.param('song');
+    if (index && index >= 0 && index < songs.length) {
+      return res.send({ 'result': MUSIC_PATH + 'kpop/' + songs[index] });
+    }
+    return res.send({ 'filenames': songs });
+  }
+};
 
+app.use(express.static(__dirname + '/public', { maxAge: 86400000 }));
+app.all('/playlist', Controller.handlePlaylist);
+
+  
 console.log("Server running at port " + PORT);
 app.listen(PORT);
